06c08322af5d0f70cadbf33b9b353584dd824d7d
Muhammed Emir IŞIK <03emire@gmail.com>
2025-08-06 01:48:19 +0300

    a

--- Program.cs
+++ Program.cs
@@ -11,7 +11,9 @@ namespace DikenEngine
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+            //MessageBox.Show("Bilinmeyen bir hata oluþtu.", "DikenEngine-C# Clash", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            DikenEngine.Engine.Create();
         }
     }
 }
